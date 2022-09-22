933b94dbd48c23ea2aaf15675cec21f4b1a065c2
ZioBeppe99 <fanciulli.giuseppe@gmail.com>
2022-09-22 15:20:37 +0200

    Initial import.

--- bookstats.py
+++ bookstats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2022 luca.baldini@pi.infn.it
+# Copyright (C) 2022 g.fanciulli2@studenti.unipi.it
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
