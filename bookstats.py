b412541d50f0c708c6b05b15d40377c89e657b7d
ZioBeppe99 <fanciulli.giuseppe@gmail.com>
2022-09-24 20:04:59 +0200

    Initial import.

--- bookstats.py
+++ bookstats.py
@@ -18,21 +18,40 @@
 """
 
 import argparse
-
+import time
+import numpy as np
+from matplotlib import pyplot as plt
 
 def process(file_path):
-    """
-    """
+    """Gives back the relative frequency of letters in a book"""
     print(f'Opening input file {file_path}...')
     with open(file_path, 'r') as input_file:
         text = input_file.read()
-    print(text)
-    print('Done.')
+    letters = []
+    total = 0.
+    for i in range(97, 123):
+        lower_letter = text.count(chr(i))
+        upper_letter = text.count(chr(i-32))
+        increase = lower_letter+upper_letter
+        letters.append(increase)
+        total += increase
+        #print(f'Number of {chr(i-32)}\'s counted = ', n+m)
+    for i in range(97, 123):
+        letters[i-97] /= total
+        print(f'Relative frequency of {chr(i-32)}\'s = ', letters[i-97])
+    alphabet = list(map(chr, range(97, 123)))
+    arr = np.array(letters)
+    plt.bar(alphabet, arr)
+    plt.title('Letters relative frequencies of occurrance')
+    plt.show()
 
-
+START = time.time()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Print some book statistics')
-    parser.add_argument('infile', type=str, help='path to the input file')
-    args = parser.parse_args()
-    process(args.infile)
+    PARSER = argparse.ArgumentParser(description='Print some book statistics')
+    PARSER.add_argument('infile', type=str, help='path to the input file')
+    ARGS = PARSER.parse_args()
+    process(ARGS.infile)
+
+END = time.time() - START
+print(f'Time elapsed from start = {END} seconds')
